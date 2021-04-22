ee4c2294b8c79ca26d268ed39a082bc31f016157
abdul <abdul.tanveer@gmail.com>
2021-04-22 15:53:15 +0530

    datasource mysql connection -- update

--- springjdbc/src/basic/PlayJdbc.java
+++ springjdbc/src/basic/PlayJdbc.java
@@ -1,11 +1,15 @@
 package basic;
 
+import basic.dao.StudentDao;
+import basic.dao.StudentDaoImpl;
 import basic.model.Student;
 
 public class PlayJdbc {
 	public static void main(String[] args) {
 		Student myStudent = new Student(3, "akshay", 5, 66);
 		Student anotherStudent = new Student(4,"gayathri",6,88);
+		StudentDao studentDao = new StudentDaoImpl();
+		studentDao.insert(anotherStudent);
 	}
 
 }
--- springjdbc/src/basic/dao/StudentDaoImpl.java
+++ springjdbc/src/basic/dao/StudentDaoImpl.java
@@ -1,13 +1,35 @@
 package basic.dao;
 
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
 import basic.model.Student;
 
 public class StudentDaoImpl  implements StudentDao{
+	private JdbcTemplate jdbcTemplate = new JdbcTemplate(getDataSource());
+
 
 	@Override
 	public void insert(Student student) {
-		// TODO Auto-generated method stub
-		
+		//INSERT INTO `student` (`_id`, `name`, `semester`, `average`) 
+		//VALUES ('9', 'yahas', '2', '67');
+		String sql = "INSERT INTO student VALUES (?,?,?,?)";
+		Object[] objects =	{student.getId(),student.getName(),student.getSem(),student.getAverage()};
+
+		int no_rows_inserted =	jdbcTemplate.update(sql,objects);
+		System.out.println("no of rows inserted  is"+ no_rows_inserted);
+	}
+
+
+	public DataSource getDataSource() {
+		String url = "jdbc:mysql://127.0.0.1:3306/spring_jdbc";
+		String username = "root";
+		String password ="";
+
+		DriverManagerDataSource dataSource = new DriverManagerDataSource(url, username, password);
+		return dataSource;
 	}
 
 }
